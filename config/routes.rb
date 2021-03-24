80616e77e446dd07036e69a805b971d60768d76f
SLim <sam@samlim.com>
2021-03-23 21:02:57 -0400

    some unfinished views

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :items
   resources :trips do
     resources :invitations, only: [:create]
   end
   resources :invitations, only: [:update]
-  resources :trips, only: [:edit, :update]
+  resources :trips, only: [:new, :edit, :update]
 end
