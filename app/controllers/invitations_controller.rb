a5525b27f6ecc9bfb550808b53b31497958a490e
Qiyun Dai <pianoquin@gmail.com>
2021-03-26 12:15:35 -0500

    controller tentatively finished. Need to add 'email' and 'status' columns to invitations table

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,2 +1,37 @@
 class InvitationsController < ApplicationController
+  before_action :set_invitation, only: [:accept, :update, :destroy]
+
+  def new
+    @invitation = Invitation.new
+    authorize @invitation
+  end
+  
+  def create
+    @trip = Trip.find(params[:trip_id])
+    @invitation = Invitation.create(invitation_params)
+  end
+
+  def accept
+    participant = current_user
+    @invitation.user = participant
+  end
+
+  def update
+    @invitation.status = params[:status]
+  end
+
+  def destroy
+    @invitation.destroy
+    redirect_to trip_page(@invitation.trip)
+  end
+
+  private
+
+  def invitation_params
+    params.require(:invitation).permit(:email)
+  end
+
+  def set_invitation
+    @invitation = Invitation.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :items
   resources :trips do
-    resources :invitations, only: [:create]
+    resources :invitations, only: [:new, :create]
   end
   resources :invitations, only: [:update]
-  resources :trips, only: [:edit, :update]
+  resources :trips, only: [:new, :edit, :update]
 end
