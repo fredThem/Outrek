1c68c414ac947a919103eb6fbb99e269e537d9a3
Qiyun Dai <pianoquin@gmail.com>
2021-03-27 13:35:51 -0500

    quick controller and activity bug fix

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,6 +1,6 @@
 class Activity < ApplicationRecord
   has_many :recommended_item_labels
-  belongs_to :trip, through: :trip_activities
+  has_many :trip_activities
 
   validates :name, presence: true
 end
