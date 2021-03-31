301e5d6c312964436b2a89d46747c4b61d58d0ac
Frederic Desmarais <fdesmarais.them@gmail.com>
2021-03-31 17:54:27 -0400

    webpack scss hot reload functionalities
    run > webpack-dev-server
    and save twice

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -3,3 +3,14 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 const environment = require('./environment')
 
 module.exports = environment.toWebpackConfig()
+
+const chokidar = require("chokidar");
+environment.config.devServer.before = (app, server) => {
+  chokidar
+    .watch([
+      "config/locales/**/*.yml",
+      "app/views/**/*.html.erb",
+      "app/assets/**/*.scss",
+    ])
+    .on("change", () => server.sockWrite(server.sockets, "content-changed"));
+}
