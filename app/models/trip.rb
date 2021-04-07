c867d670d32b95cb22dbd178882fdad235019937
Qiyun Dai <pianoquin@gmail.com>
2021-04-07 14:41:39 -0500

    imported Cloudinary key and added has_one_attached to Trip

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -5,6 +5,7 @@ class Trip < ApplicationRecord
   has_many :invitations, dependent: :destroy
   has_many :trip_activities, dependent: :destroy
   has_many :activities, through: :trip_activities
+  has_one_attached :map_image
 
   validates :destination, presence: true
   validates :description, presence: true,
