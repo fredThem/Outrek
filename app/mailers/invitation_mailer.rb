ace90002ae4878074bca395bb2907385108f185f
Qiyun Dai <pianoquin@gmail.com>
2021-04-13 20:42:59 -0500

    back to https

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -3,7 +3,7 @@ class InvitationMailer < ApplicationMailer
 
   def invite_email
     @email = params[:email]
-    @url = "http://outrek.app/invitations/#{params[:invitation].id}/accept"
+    @url = "https://outrek.app/invitations/#{params[:invitation].id}/accept"
     @trip = params[:trip]
     mail(to: @email, subject: "Outreker! You've Been Invited!")
   end
