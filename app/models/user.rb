91bb7a3c162f8958c12daf3d70150c29f0ca50ab
fredThem <fdesmarais.them@gmail.com>
2021-04-15 14:24:13 -0400

    attaching default avatars

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  
+
   has_many :trips, dependent: :destroy
   has_many :invitations, dependent: :destroy
   has_many :items, dependent: :destroy
@@ -11,10 +13,23 @@ class User < ApplicationRecord
   has_many :checklists, through: :trips
   has_many :duties, dependent: :destroy
   has_many :checklist_items, through: :duties
+
   has_one_attached :avatar
+  after_commit :add_default_avatar, on: :create
 
   has_person_name
   validates :first_name, presence: true
   validates :last_name, presence: true
   # validates :avatar, presence: true
+
+  private 
+  
+  def add_default_avatar
+    unless avatar.attached?
+      puts "attaching default user avatar"
+      default_avatars = Dir['app/assets/images/avatars-default/*'].size
+      avatar.attach(io: File.open(Rails.root.join("app", "assets", "images", "avatars-default", "avatar-critters_#{rand(default_avatars)}.png")),
+                   filename: 'default_avatar.jpg', content_type: "image/png")
+    end
+  end
 end
