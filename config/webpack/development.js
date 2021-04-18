1e8d45be2100706fd2ae3928ead6701ba1d8f547
fredThem <fdesmarais.them@gmail.com>
2021-04-18 07:10:10 -0400

    Update development.js
    this is the end of chokidar, so long *let's keep as reference

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -5,13 +5,13 @@ const environment = require('./environment')
 module.exports = environment.toWebpackConfig()
 
 // IMPORTANT this is to enable scss hotreload - requires to run webpack-dev-server
-const chokidar = require("chokidar");
-environment.config.devServer.before = (app, server) => {
-  chokidar
-    .watch([
-      "config/locales/**/*.yml",
-      "app/views/**/*.html.erb",
-      "app/assets/**/*.scss",
-    ])
-    .on("change", () => server.sockWrite(server.sockets, "content-changed"));
-}
+  // const chokidar = require("chokidar");
+  // environment.config.devServer.before = (app, server) => {
+  //   chokidar
+  //     .watch([
+  //       "config/locales/**/*.yml",
+  //       "app/views/**/*.html.erb",
+  //       "app/assets/**/*.scss",
+  //     ])
+  //     .on("change", () => server.sockWrite(server.sockets, "content-changed"));
+  // }
