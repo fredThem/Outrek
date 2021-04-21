36fadf297ea5e3e0553ca30ab14b19c5ddd135ef
fredThem <fdesmarais.them@gmail.com>
2021-04-20 22:22:03 -0400

    init

--- app/javascript/plugins/init_tooltip.js
+++ app/javascript/plugins/init_tooltip.js
@@ -1,6 +1,14 @@
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl)
-})
+// var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+// var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+//   return new bootstrap.Tooltip(tooltipTriggerEl)
+// })
+
+const tooltipList = () => {
+  $(function () {
+    $('[data-bs-toggle="tooltip"]').tooltip()
+  })
+}
+
+// export { tooltipList };
 
 export { tooltipList };
