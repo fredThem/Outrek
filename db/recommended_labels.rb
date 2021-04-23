1f9cdb1a88a64d92e3f8022c585c0c939c5ec6b0
fredThem <fdesmarais.them@gmail.com>
2021-04-23 10:57:34 -0400

    Create recommended_labels.rb

---
+++ db/recommended_labels.rb
@@ -0,0 +1,224 @@
+# based on :
+# https://www.rei.com/learn/expert-advice
+
+the_ten_essential_systems = [ #https://www.rei.com/learn/expert-advice/ten-essentials.html
+  "The Ten Essential Systems": {
+    "Navigation: map, compass, altimeter, GPS device, personal locator beacon (PLB) or satellite messenger",
+    "Headlamp: plus extra batteries",
+    "Sun protection: sunglasses, sun-protective clothes and sunscreen",
+    "First aid including foot care and insect repellent (as needed)",
+    "Knife plus a gear repair kit",
+    "Fire matches, lighter, tinder and/or stove",
+    "Shelter carried at all times (can be a light emergency bivy)",
+    "Extra food Beyond the minimum expectation",
+    "Extra water Beyond the minimum expectation",
+    "Extra clothes Beyond the minimum expectation"
+  }
+]
+
+first_aid_kit = [ # https://www.rei.com/learn/expert-advice/first-aid-checklist.html
+  "First-Aid": {
+    "First-Aid Basic Care": {
+      "Antiseptic wipes (BZK-based wipes preferred; alcohol-based OK)",
+      "Antibacterial ointment (e.g., bacitracin)",
+      "Compound tincture of benzoin (bandage adhesive)",
+      "Assorted adhesive bandages (fabric preferred)",
+      "Butterfly bandages / adhesive wound-closure strips",
+      "Gauze pads (various sizes)",
+      "Nonstick sterile pads",
+      "Medical adhesive tape (10 yd. roll, min. 1inch width)",
+      "Blister treatment",
+      "Ibuprofen / other pain-relief medication",
+      "Insect sting / anti-itch treatment",
+      "Antihistamine to treat allergic reactions",
+      "Splinter (fine-point) tweezers",
+      "Safety pins",
+      "First-aid manual or information cards"
+    }
+  }
+]
+
+curated_recommendations = [
+  # "Backpacking",
+  "Biking": [ #https://www.rei.com/learn/c/cycling/t/checklists?ir=category%3Acycling&r=c%3Bt
+    "Bikepacking": {  #https://www.rei.com/learn/expert-advice/bikepacking-checklist.html
+      "",
+    },
+    "Bike Touring": {
+      "",
+    },
+    "Mountain Biking": {
+      "",
+    },
+    "Road Cycling": {
+      "",
+    }
+  ]
+  "Hiking": [ # https://www.rei.com/learn/c/hiking/t/checklists
+    "Day Hiking": { # https://www.rei.com/learn/expert-advice/day-hiking-checklist.html
+      ""
+    },
+    "Ultralight Backpacking": { #https://www.rei.com/learn/expert-advice/ultralight-backpacking-checklist.html
+      ""
+    }
+  ],
+  ### "Photography",
+  "Paddle Sports": [
+    "Canoeing": { #https://www.rei.com/learn/expert-advice/canoe-day-touring-checklist.html
+      "Canoe Basics": {
+        "Canoe",
+        "Paddle (1 per paddler)",
+        "Extra paddle",
+        "Personal flotation device (1 per paddler)",
+        "Extra PFD",
+        "Seat",
+        "Knee pads",
+        "Yoke (with pads)",
+        "Dry bags (for gear organization)",
+        "Spray covers",
+        "Weather/VHF radio",
+        "Signaling devices (whistle, mirror, flares)",
+        "Float bags",
+        "Emergency floating throw line (with throw bag)",
+        "Bailer or bilge pump",
+        "Sponges"
+      },
+      "Core Gear": { # category
+        "Maps and charts in waterproof case", #name
+        "Compass",
+        "GPS",
+        "Matches/lighter/fire starter in waterproof container",
+        "Knife or multi-tool",
+        "2-way radios",
+        "Cell phone in protective bag",
+        "Multifunction watch",
+        "Headlamp or flashlight (with extra batteries)",
+        "First-aid supplies (see our First-Aid Checklist)",
+        "Sunglasses",
+        "Sunscreen",
+        "Lip balm",
+        "Water bottles (filled)",
+        "Water treatment method (and backup)",
+        "Energy food (bars, gels, trail mix)",
+        "Beverages or powdered drinks"
+      },
+      "Clothing": {
+        "Dry suit or top",
+        "Paddling gloves or pogies",
+        "Neoprene footwear",
+        "Sandals",
+        "Helmet",
+        "Sun-shielding hat",
+        "Hat or cap retainer",
+        "Skullcap",
+        "Bandana or buff",
+        "Wool/synthetic cap or balaclava",
+        "Moisture-wicking T-shirt",
+        "Moisture-wicking underwear",
+        "Rashguard",
+        "Swimsuit",
+        "Shorts or convertible pants",
+        "Sun-shielding shirt (long sleeves) and pants",
+        "Spray jacket or rainwear (jacket and pants)",
+        "Synthetic vest or jacket (for insulation)",
+        "Synthetic gloves or mittens"
+      },
+      "Repair-Kit Options": {
+        "Sealant (such as Aquaseal)",
+        "Bailing wire (copper, small roll)",
+        "Nylon cord (or bungee cords)",
+        "Putty",
+        "Replacement nuts/bolts",
+        "Repair/duct tape"
+      },
+      "Other Day Gear Options": {
+        "Lunch",
+        "Energy food (bars, gels, trail mix)",
+        "Beverages or powdered drinks",
+        "Cooler (with ice)",
+        "Camera",
+        "Binoculars",
+        "Duffels (to supplement dry bags)",
+        "Insect repellent",
+        "Toilet paper and sanitation trowel",
+        "Permits and licenses",
+        "Fishing gear",
+        "Notebook and pen or pencil",
+        "Credit card; small amount of cash",
+        "Trip itinerary left with friend + under car seat"
+      }
+    },
+    "Stand Up Paddle Boarding (SUP)": {
+      "Essential Gear": {
+        "Stand up paddle board (SUP)",
+        "SUP paddle",
+        "Personal flotation device (PFD)",
+        "Pump (for inflatable SUP)",
+        "Fin(s) for SUP",
+        "SUP leash",
+        "Rescue whistle",
+        "Headlamp or flashlight (required if paddling after sundown)"
+      },
+      "Clothing and Footwear for warm weather and water (>70°F)": {
+        "Rashguard or other sun-protective shirt",
+        "Board shorts or swimsuit",
+        "Water shoes, neoprene booties or sandals",
+        "Sun-shielding hat",
+        "Hat retainer leash (optional)"
+      },
+      "Clothing and Footwear for cold weather and water (<60°F)": {
+        "Neoprene top and shorts, or Wetsuit",
+        "Paddling gloves",
+        "Wool/synthetic cap",
+        "Wool/synthetic socks"
+      },
+      "Recommended Personal Items": {
+        "Sunscreen (water-resistant SPF 30+) ",
+        "Lip balm (SPF 15+)",
+        "Sunglasses and straps",
+        "Water bottle or hydration reservoir or waistpack (filled)",
+        "Snacks or lunch",
+        "Credit card; small amount of cash",
+        "Cellphone in protective bag/case",
+        "Small first-aid kit",
+        "Insect repellent (as needed)",
+        "Camera with waterproof case",
+        "Towel"
+      },
+      "Optional Gear Items": {
+        "Towline",
+        "Deck bag and/or dry bags",
+        "Camera mount",
+        "GPS",
+        "Two-way radios",
+        "Weather/VHF radio",
+        "Guidebook",
+        "Maps and charts in waterproof case",
+        "Compass (that floats)",
+        "Knife",
+        "Fishing gear",
+        "SUP carry strap",
+        "SUP transport cart"
+      }
+    },
+    "Kayaking"
+  ],
+  "Climbing": [ # FOR FUTURE IMPLEMENTATION
+    "Bouldering": {
+      ""
+    },
+    "Ice Climbing": { # https://www.vertical-addiction.com/us/blogs/blog/ice-climbing-packing-list-the-essentials/
+      ""
+    },
+    "Sport Climbing": { # https://www.rei.com/learn/expert-advice/sport-climbing-checklist.html
+      ""
+    },
+    "Trad Climbing": {  # https://www.rei.com/learn/expert-advice/climbing-trad-checklist.html?cm_mmc=sm_pin-_-outside-everyday&crlt.pid=camp.2RkFP5ynxPDl
+      ""
+    },
+  ],
+  ### "Scenic Driving",
+  ### "Skiing",
+  ### "Snowshoeing",
+  ### "Trail Running",
+]
